b84e4b11e67a86de73a2fa9a08d4505dd85c968b
Omar Andrés Díaz <omitar282@gmail.com>
2022-05-01 11:08:20 -0500

    3

---
+++ Ejemplo2_VS/comandos/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+// VARIABLES ------------------------------------------
+string Cadena="El número es = ";
+string Cadena2="El ascii es = ";
+int Limite=0;
+double suma=0.1;
+// COMANDOS ------------------------------------------
+Console.WriteLine("Digite un número: ");
+Limite=Console.Read();
+
+Console.WriteLine(Cadena2+Limite);
+
+for(double i=0; i<Limite; i++){
+    Console.WriteLine(Cadena+i);
+}
---
+++ Ejercicio_1/Ejercicio1/Program.cs
@@ -0,0 +1,31 @@
+﻿//----------------------------------------------------------------
+Console.Clear();
+//-Variables------------------------------------------------------
+int[,] Matriz = new int [,] 
+{
+    {0,0,0},
+    {0,0,0},
+    {0,0,0}
+};
+string Texto="";
+while (true){
+Console.Clear();
+Console.WriteLine("Bienvenido a multipliación de matriz.\n Seleccione la fila 1, 2 o 3");
+string Seleccion=Console.Read();
+
+if (Seleccion=="1"){
+Console.WriteLine("Fila 1 \n Inserte los valores\nValor A");
+Matriz[0,0]=Convert.ToInt32(Console.Read());
+Console.WriteLine("Valor B");
+Matriz[0,1]=Convert.ToInt32(Console.Read());
+}
+//-Imprimir---------------------------------------------------------------------
+    for (int fila=0;fila<3;fila++){ 
+        for(int columna=0;columna<3;columna++){
+           Texto=Texto+Matriz[fila,columna]+"    ";
+        }
+        Console.WriteLine(Texto);
+        Texto="";
+    }
+//-------------------------------------------------------------------------------
+}
