0dd3bd35e671d02f148ba1e9fc70740d5150b1ed
Omar Andrés Díaz <omitar282@gmail.com>
2022-05-02 08:42:14 -0500

    Proyecto 1
    Algunos cambios en las variables del inventario.

--- Proyecto_1/Inv.js
+++ Proyecto_1/Inv.js
@@ -4,16 +4,30 @@ Inventario=['Inventario:',
             'Carné de estudiante',
             'Caja',
             'California',
-            'Ladrillo',
+            'Tablero',
             '0.0000000001 BTC',
+            'Esto no es una pipa',
+            'Prueba de Inventario',
+            'Madera',
+            '😎',
+            'Pata de cabra',
+            'Dignidad',
+            'Ascensor'
         ];
 Informacion=['Información:',
             'Un producto referencia a Breaking Bad para cocinar cristales coloridos.',
             'Un simple carné de estudiante. Podría ser de cualquier universidad...',
-            'Una simple caja con nada dentro. Quizá deba devolversela a Pandora... ',
+            'Una simple caja. No había nada dentro, quizá deba devolversela a Pandora... ',
             'Todo el estado de California. A ver si los Red Hot Chili Peppers cantan sobre el algún día...',
-            'Un simple ladrillo. Pesa aproximadamente 2 kgs, no me pregunten cómo sé su peso.',
+            'Un tablero de ajedrez para escribir sobre él con marcador permanente. Como debe ser...',
             'Me encontré un bitcoin tirado en el piso. Seguro valga más en 2 años...',
+            'La famosa pipa. ¡Cómo me reprochó la gente por ello! Y sin embargo, ¿podría usted rellenarla? No, claro, es una mera representación. ¡Si hubiera escrito en el cuadro «Esto es una pipa», habría estado mintiendo!',
+            'Nada más que una simple y sencilla prueba de inventario... ¿Qué esperaban?',
+            'Un tronco de madera sin procesar. Dicen que el número de círculos determina su edad, lástima que sea un cuadrado...',
+            '😎 La calidad no se improvisa, aún en lo más sencillo, es el resultado innegable del apego a la excelencia. 😎',
+            '¿En serio quieren saber para qué guardamos la pata de una cabra en nuestro inventario?',
+            'La dignidad de alguien ha terminado en este inventario, quién sabe de quién sea, quizá olvidó su carné de estudiante también.',
+            'Un ascensor sin botones ni motores... ni personas. Realmente no sirve para nada pero ahora es nuestro.'
         ];
 //-----------------------------------------------------------------------------------------------------------
 module.exports = {Inventario, Informacion};
